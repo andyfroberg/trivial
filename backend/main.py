da333398e3021ccab27476c4d9da31d869ea3166
andrewfroberg <andy.froberg@gmail.com>
2024-04-23 22:34:39 -0700

    learning supabase

--- backend/main.py
+++ backend/main.py
@@ -2,26 +2,87 @@ from typing import Union
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+from dotenv import load_dotenv
+
+
+import os
+from supabase import create_client, Client
+
+load_dotenv()
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
+
+# class Item(BaseModel):
+#     name: str
+#     price: float
+#     is_offer: bool | None = None
+
+
+
+
+# class User(BaseModel):
+#     id: int
+#     name: str
+#     email: str
+
+# class Round(BaseModel):
+#     id: int
+#     questions: list[Questions]
+
+
+# class UserPreferences(BaseModel):
+#     ...
+
+# class Question(BaseModel):
+#     id: int
+#     category: str
+#     difficulty: int
+#     text: str
+#     correct_answer: Answer.id
+
+# class Answer(BaseModel):
+#     id: int
+#     text: str
+#     question: list[Question.id]
+
+# class TriviaQuestion(BaseModel):
+#     id: int
+#     user: User
+#     answered_correctly: bool
+
+
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    response = supabase.table('test_table').select("*").execute()
+    return {"response": response}
+
+@app.get("/signiup")
+def read_signup():
+    pass
+
+@app.post("/signup")
+def create_user():
+    res = supabase.auth.sign_up(
+        email= "",
+        password= "",
+    )
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
 
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+# @app.put("/items/{item_id}")
+# def update_item(item_id: int, item: Item):
+#     return {"item_name": item.name, "item_id": item_id}
+
